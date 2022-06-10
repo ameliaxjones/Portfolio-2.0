c3288970c94324871c98e1ba8bc5c56e319b5af6
Amelia <gcab022111@coderacademy.edu.au>
2022-06-10 10:31:56 +1000

    styled error page, added error border and error icon

--- src/pages/ErrorPage.js
+++ src/pages/ErrorPage.js
@@ -1,14 +1,16 @@
 import React from "react";
+import Draggable from "react-draggable";
+import "../ErrorPage.css";
 
-
-function ErrorPage(){
-    return (
-
-        <div> 
-            <p>Error. Page doesnt exsist</p>
-        </div>
-    )
-
+function ErrorPage() {
+  return (
+    <Draggable>
+      <div id="error-border">
+      <img id="error-icon" src="assets/erroricon.png" />
+        <p id="txt-error">Error: Page Not Found.</p>
+      </div>
+    </Draggable>
+  );
 }
 
-export default ErrorPage
+export default ErrorPage;
