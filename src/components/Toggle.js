25405ea523fca0d76eac3ba15e089e3208572bbc
Amelia <gcab022111@coderacademy.edu.au>
2022-06-11 11:38:16 +1000

    added back draggle with bug fix

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,47 +1,44 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import Draggable from "react-draggable";
 import "../About.css";
 
-
 const Toggle = () => {
   return (
+    <div>
       <div>
-          <div>
-            
-          
-              <div id="contentbox"> 
-                <div id="content">
-                  <div className="scrollable-div-about">
-                    <img id="about-me" src="assets/desksmall.png" />
+        <Draggable cancel=".cancel">
+          <div id="contentbox">
+            <div id="content">
+              <div className=" cancel scrollable-div-about">
+                <img id="about-me" src="assets/desksmall.png" />
+                <p>
+                  I'm Amelia I am a junior web developer currently living in
+                  Brisbane, Australia. I have always enjoyed creating things
+                  from hacking together my myspace page, to drawing and
+                  painting.{" "}
+                </p>
+                <p>
+                  {" "}
+                  I am a well-organised person, a problem solver with high
+                  attention to detail who is always up for a challenge. On my
+                  days off I enjoy art, messing around with Unity, hanging out
+                  with my dog Leeroy and playing video games.
+                  <div id="bottom-border">---</div>
+                  <div id="video-game">
+                    <p>Video game of the month: V Rising. </p>
                     <p>
-                      I'm Amelia I am a junior web developer currently living in
-                      Brisbane, Australia. I have always enjoyed creating things
-                      from hacking together my myspace page, to drawing and
-                      painting.{" "}
-                    </p>
-                    <p>
-                      {" "}
-                      I am a well-organised person, a problem solver with high
-                      attention to detail who is always up for a challenge. On
-                      my days off I enjoy art, messing around with Unity,
-                      hanging out with my dog Leeroy and playing video games.
-                      <div id="bottom-border">---</div>
-                      <div id="video-game">
-                        <p>Video game of the month: V Rising. </p>
-                        <p>
-                          If you like survival games with the added bonus of
-                          being a vampire, building your own castle then
-                          definitely check this game out!
-                        </p>
-                      </div>
+                      If you like survival games with the added bonus of being a
+                      vampire, building your own castle then definitely check
+                      this game out!
                     </p>
                   </div>
-                </div>
+                </p>
               </div>
-            
-            
+            </div>
           </div>
+        </Draggable>
       </div>
+    </div>
   );
 };
 
